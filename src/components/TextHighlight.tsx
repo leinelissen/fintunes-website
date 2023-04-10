62c3acbf22f40b878719cc305fc3f65efd5dab44
Lei Nelissen <lei@codified.nl>
2023-04-10 22:14:37 +0200

    fix: prefix background-clip

--- src/components/TextHighlight.tsx
+++ src/components/TextHighlight.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const TextHighlight = styled.span`
     background-image: -webkit-linear-gradient(53deg, var(--gradient-1), var(--gradient-2));
     background-clip: text;
+    -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
     font-weight: bold;
     font-variation-settings: "wght" 700;
