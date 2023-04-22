8a1b1c87da600373f8d938c77613006a70711277
Lei Nelissen <lei@codified.nl>
2023-04-22 20:29:19 +0200

    feat: add verccel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/variable.css';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Component {...pageProps} />
+            <Analytics />
+        </>
+    );
 }
